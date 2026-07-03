50dbdc7fdaf9d8d89cf812982e8b522c14ec01a1
Milagros Alvarado <milagrosalvarado1248@gmail.com>
2026-07-02 23:12:27 -0300

    "se creó el modelo IndiceIpc"

---
+++ InterFacil/src/types/ipc.ts
@@ -0,0 +1,10 @@
+export interface IndiceIpc {
+  id: string;
+  mes: string;
+  año: number;
+  porcentaje: number;
+  acumulado: number;
+  fuente: string;
+  publicadoEl: string;
+  categoria: string;
+}
